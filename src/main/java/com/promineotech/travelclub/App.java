daf8ac5d09ec91b83d6c7be1cb8ace14bc878c66
matthewblaire <49771927+matthewblaire@users.noreply.github.com>
2020-12-09 18:48:50 -0700

    Added pom.xml

--- src/main/java/com/promineotech/travelclub/App.java
+++ src/main/java/com/promineotech/travelclub/App.java
@@ -4,10 +4,16 @@ package com.promineotech.travelclub;
  * Hello world!
  *
  */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.promineotech.travelclub")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
