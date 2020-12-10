2a28219ac13fc3ce798b56a8c2571d6af14e9a7c
matthewblaire <49771927+matthewblaire@users.noreply.github.com>
2020-12-09 18:17:31 -0700

    Initial commit

---
+++ src/test/java/com/promineotech/travelclub/AppTest.java
@@ -0,0 +1,20 @@
+package com.promineotech.travelclub;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
