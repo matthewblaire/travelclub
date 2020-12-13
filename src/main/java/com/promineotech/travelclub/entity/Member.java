53249089663e786b96f1bd82662e475a522ec986
BritneyHanna <b.ahanna@yahoo.com>
2020-12-13 12:43:46 -0500

    initial commit Member entity

--- src/main/java/com/promineotech/travelclub/entity/Member.java
+++ src/main/java/com/promineotech/travelclub/entity/Member.java
@@ -1,5 +1,86 @@
 package com.promineotech.travelclub.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+
+	private Long id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private MembershipLevel level; // need to create enum membership level
+	private int miles;
+	private String phoneNumber;
+	private String email;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public MembershipLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(MembershipLevel level) {
+		this.level = level;
+	}
+
+	public int getMiles() {
+		return miles;
+	}
+
+	public void setMiles(int miles) {
+		this.miles = miles;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
