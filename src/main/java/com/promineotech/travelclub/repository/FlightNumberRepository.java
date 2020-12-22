09f88d410520676fe8c6462d510b070363057054
BritneyHanna <b.ahanna@yahoo.com>
2020-12-22 09:33:54 -0500

    added repository layer

---
+++ src/main/java/com/promineotech/travelclub/repository/FlightNumberRepository.java
@@ -0,0 +1,5 @@
+package com.promineotech.travelclub.repository;
+
+public interface FlightNumberRepository {
+
+}
---
+++ src/main/java/com/promineotech/travelclub/repository/LoyaltyClubRepository.java
@@ -0,0 +1,9 @@
+package com.promineotech.travelclub.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.promineotech.travelclub.entity.LoyaltyClub;
+
+public interface LoyaltyClubRepository extends CrudRepository<LoyaltyClub, Long> {
+
+}
