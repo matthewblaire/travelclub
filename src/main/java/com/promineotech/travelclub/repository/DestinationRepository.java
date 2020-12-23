afe3038975d1eb0f4a31de0b0f135976504e2c71
matthewblaire <49771927+matthewblaire@users.noreply.github.com>
2020-12-22 19:09:07 -0700

    Added Destination service

--- src/main/java/com/promineotech/travelclub/repository/DestinationRepository.java
+++ src/main/java/com/promineotech/travelclub/repository/DestinationRepository.java
@@ -1,9 +1,13 @@
 package com.promineotech.travelclub.repository;
 
+
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.promineotech.travelclub.entity.Destination;
 
 public interface DestinationRepository extends CrudRepository<Destination, Long> {
-
+	
+	
 }
+
--- src/main/java/com/promineotech/travelclub/service/DestinationService.java
+++ src/main/java/com/promineotech/travelclub/service/DestinationService.java
@@ -1,5 +1,51 @@
 package com.promineotech.travelclub.service;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.promineotech.travelclub.entity.Destination;
+import com.promineotech.travelclub.repository.DestinationRepository;
+
+@Service
 public class DestinationService {
-
+	
+	private static final Logger logger = LogManager.getLogger(DestinationService.class);
+	
+	@Autowired
+	private DestinationRepository repo;
+	
+	public Iterable<Destination> getDestinations() {
+		return repo.findAll();
+	}
+	
+	public Destination createDestination(Destination destination) {
+		return repo.save(destination);
+	}
+	
+	public Destination updateDestination(Destination destination, Long id) throws Exception {
+		
+		try {
+			Destination oldDestination = repo.findById(id).orElse(null);
+			oldDestination.setCity(destination.getCity());
+			oldDestination.setCode(destination.getCode());
+			oldDestination.setCountry(destination.getCountry());
+			oldDestination.setState(destination.getState());
+			return repo.save(oldDestination);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to update destination: " + id, e);
+			throw new Exception("Unable to update destination.");
+		}
+		
+	}
+	
+	public void removeDestination(Long id) throws Exception {
+		try {
+			repo.deleteById(id);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to delete destination: " + id, e);
+			throw new Exception("Unable to delete destination.");
+		}
+	}
+	
 }
